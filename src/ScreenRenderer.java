a24361220754834519a5b2ea369613455ba70b60
Ryan <Ryan@10.0.0.189>
2019-11-03 12:02:03 -0500

    exit on close?

---
+++ src/ScreenRenderer.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage;
+
+public class ScreenRenderer {
+	
+	public float cameraLocalX;
+	public float cameraLocalY;
+	
+	public 
+	
+	
+	void addSprite(BufferedImage img, int x, int y) {
+		
+	}
+	
+}
